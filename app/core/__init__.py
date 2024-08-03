aa13bb0666283247a0303cc5afa85887fa0dbe08
Ayou .w. <heyyutop5@gmail.com>
2024-08-03 09:43:57 +0800

    模組化

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,6 +1,11 @@
-# class Form():
-#     def __init__(self):
-#         self.founder = founder
-#         self.option = option
-#         self.num = num
-#         self.values = []
+class Choice():
+    def __init__(self,founder, option_num, num):
+        self.founder = founder
+        self.option_num = option_num
+        self.num = num
+        self.option_name = []
+        self.people = {}
+        self.result = []
+    def to_dict(self):
+        return self.__dict__
+
